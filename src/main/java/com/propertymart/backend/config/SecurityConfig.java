925aa88b0aedb9fc04cbe104f11fab7607151b20
Anthony Tummillo <atummillo18@gmail.com>
2024-06-14 04:57:23 -0400

    Get POST working / resolve 403 response

--- src/main/java/com/propertymart/backend/config/SecurityConfig.java
+++ src/main/java/com/propertymart/backend/config/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.propertymart.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/**").permitAll() // Allow api access
                                 .anyRequest().authenticated() // All other requests require authentication
-                )
-                .formLogin(Customizer.withDefaults());
+                );
         return http.build();
     }
 }
